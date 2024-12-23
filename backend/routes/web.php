e2259b2212015c1dbf276d0b9e1d14112400d9be
blezz-tech <markus.jenya04@yandex.ru>
2024-12-23 20:04:31 +0300

    Добавление REST апи для приложения

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\ChatMessageController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('v1')->group(function () {
+
+
+    Route::middleware(['auth'])->group(function () {
+        Route::apiResource('user', UserController::class)->except(['index', 'store']);
+        Route::apiResource('settings', UserController::class)->except(['index', 'store']);
+        Route::apiResource('chat', ChatController::class);
+        Route::apiResource('chat.message', ChatMessageController::class)->except('show');
+        Route::apiResource('message', MessageController::class)->except(['store', 'show', 'update']);
+
+        // Route::apiResource('user.chat.users', UserChatUserController::class)->only('index');
+        // Route::apiResource('user.chat.user.message', UserChatUserMessageController::class)->only('index');
+    });
 });
